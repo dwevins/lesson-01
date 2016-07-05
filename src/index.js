4537b97122eafe6935c1f46efdc9470e8a551353
Danny Evins <dcastkid@gmail.com>
2016-07-05 12:00:24 -0500

    buttons work now, making api calls

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 export default function (app) {
   const upBtn = app.querySelector('.btn--up');
   const dnBtn = app.querySelector('.btn--down');
+  const count = app.querySelector('.counter__count');
+
+  let number = 1;
+
+  function update() {
+    count.innerText = number;
+  }
 
   function upClick() {
-    console.log('upClick');
+    number += 1;
+    update();
   }
 
   function dnClick() {
-    console.log('dnClick');
+    number -= 1;
+    update();
   }
 
   upBtn.addEventListener('click', upClick);
   dnBtn.addEventListener('click', dnClick);
+
+  const later = fetch('https://swapi.co/api/people/1');
+  const evenLater = later.then((res) => res.json());
+  evenLater.then((data) => {
+    console.log(data);
+  });
 }
