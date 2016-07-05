d37f32d0be76b1db51a97f3f15c8a6f1062d9f36
Danny Evins <dcastkid@gmail.com>
2016-07-05 10:48:55 -0500

    add html, js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-'use strict';
+export default function (app) {
+  const upBtn = app.querySelector('.btn--up');
+  const dnBtn = app.querySelector('.btn--down');
+
+  function upClick() {
+    console.log('upClick');
+  }
+
+  function dnClick() {
+    console.log('dnClick');
+  }
+
+  upBtn.addEventListener('click', upClick);
+  dnBtn.addEventListener('click', dnClick);
+}
