6801bf741d19a4b7ea22a528218829e3e9a21a5f
Danny Evins <dcastkid@gmail.com>
2016-07-06 11:32:29 -0500

    building counter.js

--- src/counter.js
+++ src/counter.js
@@ -1,6 +1,28 @@
 export default class Counter {
-  constructor() {
+  constructor(parentElement) {
     const currentComponent = this;
+
+    this.elements = {
+      upBtn: parentElement.querySelector('.btn--up'),
+      dnBtn: parentElement.querySelector('.btn--down'),
+      count: parentElement.querySelector('.counter__count'),
+      charInfo: parentElement.querySelector('.character-info'),
+    }
+
+    this.data {
+      number: 1,
+    };
+
+    upClick() {
+      this.data.number += 1;
+      this.update();
+    },
+
+    dnClick() {
+      this.data.number -= 1;
+      this.update();
+    },
+
     this.elements.upBtn.addEventListener('click', this.actions.upClick.bind(currentComponent));
     this.elements.dnBtn.addEventListener('click', this.actions.dnClick.bind(currentComponent));
   }
